b0314d6a6513ed27d968604d4a15f356617e90f8
Cameron Alexander <emptyflash@gmail.com>
2015-07-21 22:36:13 -0700

    Redesigned display options panel, added jscsrc and reformated code

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -1,50 +1,50 @@
 var processingInstance;
 
-$( document ).ready(function() {
-	getProcessingInstance();
+$(document).ready(function() {
+    getProcessingInstance();
 });
 
 function getProcessingInstance() {
-	var bound = false;
-	processingInstance = Processing.getInstanceById('feedback');
-	if(processingInstance != null)
-		bound = true;
-	if(!bound)
-		setTimeout(getProcessingInstance, 250);
-	else
-		bindButtons();
+    var bound = false;
+    processingInstance = Processing.getInstanceById('feedback');
+    if (processingInstance != null)
+    bound = true;
+    if (!bound)
+    setTimeout(getProcessingInstance, 250);
+    else
+    bindButtons();
 };
 
 function focusCanvas() {
-	$("#feedback").attr("tabindex", 0);
-	$("#feedback").focus();
+    $("#feedback").attr("tabindex", 0);
+    $("#feedback").focus();
 };
 
 function bindButtons() {
-	$("#rotateLeftButton").mousedown(function() { 
-		processingInstance.leftPressed();
-		focusCanvas();
-	});
-	$("#rotateLeftButton").mouseup(function() { processingInstance.leftReleased(); });
-	$("#rotateRightButton").mousedown(function() { processingInstance.rightPressed(); });
-	$("#rotateRightButton").mouseup(function() { processingInstance.rightReleased(); });
+    $("#rotateLeftButton").mousedown(function() { 
+        processingInstance.leftPressed();
+        focusCanvas();
+    });
+    $("#rotateLeftButton").mouseup(function() { processingInstance.leftReleased(); });
+    $("#rotateRightButton").mousedown(function() { processingInstance.rightPressed(); });
+    $("#rotateRightButton").mouseup(function() { processingInstance.rightReleased(); });
 
-	$("#scaleUpButton").mousedown(function() { processingInstance.upPressed(); });
-	$("#scaleUpButton").mouseup(function() { processingInstance.upReleased(); });
-	$("#scaleDownButton").mousedown(function() { processingInstance.downPressed(); });
-	$("#scaleDownButton").mouseup(function() { processingInstance.downReleased(); });
+    $("#scaleUpButton").mousedown(function() { processingInstance.upPressed(); });
+    $("#scaleUpButton").mouseup(function() { processingInstance.upReleased(); });
+    $("#scaleDownButton").mousedown(function() { processingInstance.downPressed(); });
+    $("#scaleDownButton").mouseup(function() { processingInstance.downReleased(); });
 
-	$("#resetScaleButton").click(function() { processingInstance.resetScale(); });
+    $("#resetScaleButton").click(function() { processingInstance.resetScale(); });
 
-	$("#colorCheckbox").change(function() { processingInstance.toggleColorStep(); });
-	$("#autoCheckbox").change(function() { processingInstance.toggleAuto(); });
-	$("#shapeCheckbox").change(function() { processingInstance.toggleDrawShape(); });
-	$("#circleRadioButton").change(function() { processingInstance.toggleSquare(); });
-	$("#squareRadioButton").change(function() { processingInstance.toggleSquare(); });
+    $("#colorCheckbox").change(function() { processingInstance.toggleColorStep(); });
+    $("#autoCheckbox").change(function() { processingInstance.toggleAuto(); });
+    $("#shapeCheckbox").change(function() { processingInstance.toggleDrawShape(); });
+    $("#circleRadioButton").change(function() { processingInstance.toggleSquare(); });
+    $("#squareRadioButton").change(function() { processingInstance.toggleSquare(); });
 
-	$("#invertButton").click(function() { processingInstance.invert(); });
-	$("#resetButton").mousedown(function() { processingInstance.resetPressed(); });
-	$("#resetButton").mouseup(function() { processingInstance.resetReleased(); });
+    $("#invertButton").click(function() { processingInstance.invert(); });
+    $("#resetButton").mousedown(function() { processingInstance.resetPressed(); });
+    $("#resetButton").mouseup(function() { processingInstance.resetReleased(); });
 
-	$("#saveButton").click(function() { processingInstance.saveImage(); });
+    $("#saveButton").click(function() { processingInstance.saveImage(); });
 };
--- public/js/record.js
+++ public/js/record.js
@@ -1,72 +1,72 @@
-$( document ).ready(function() {
-  var canvas = $('#feedback')[0];
-  var context = canvas.getContext('2d');
-  var frameDelay = 1000/30;
+$(document).ready(function() {
+    var canvas = $('#feedback')[0];
+    var context = canvas.getContext('2d');
+    var frameDelay = 1000 / 30;
 
-  var recording = false;
-  var timer = null;
-  var frames = [];
+    var recording = false;
+    var timer = null;
+    var frames = [];
 
-  var recordButton = $("#recordButton");
-  var progressbar = $('.progress-bar');
-  var progressDiv = $('.progress');
-  progressDiv.hide();
+    var recordButton = $("#recordButton");
+    var progressbar = $('.progress-bar');
+    var progressDiv = $('.progress');
+    progressDiv.hide();
 
-  recordButton.click(function() {
-    if(!recording) {
-      gif = new GIF({
-        quality: 45,
-        workers: 4,
-        width: canvas.width,
-        height: canvas.height,
-        workerScript: 'js/gif.worker.js'
-      });
-      recording = true;
-      recordButton.text("Stop");
-      timer = setInterval(function() {
-        gif.addFrame(context, {
-          delay: frameDelay,
-          copy: true
-        });
-      }, frameDelay);
-    } else {
-      recordButton.text("Processing...");
-      clearInterval(timer);
-      progressDiv.fadeIn();
-      gif.on('progress', function(progress) {
-        progressbar.css('width', progress*100 + '%').attr('aria-valuenow', progress*100);
-        progressbar.html(Math.ceil(progress*100) + '%');
-      });
-      gif.on('finished', function(blob) {
-        recordButton.text('Record');
-        recording = false;
-        progressDiv.fadeOut();
-        progressbar.css('width', '0%').attr('aria-valuenow', 0);
-        var urlCreator = window.URL || window.webkitURL;
-        var imageUrl = urlCreator.createObjectURL(blob);
-        var reader = new FileReader();
-        reader.readAsDataURL(blob);
-        reader.onloadend = function() {
-          var displayImg = $('<img class="img-thumbnail">');
-          displayImg.css('max-width', '100%');
-          displayImg.css('max-height', '100%');
-          displayImg.css('margin', '10px 00px 10px 00px');
-          displayImg.attr('src', reader.result);
+    recordButton.click(function() {
+        if (!recording) {
+            gif = new GIF({
+                quality: 45,
+                workers: 4,
+                width: canvas.width,
+                height: canvas.height,
+                workerScript: 'js/gif.worker.js'
+            });
+            recording = true;
+            recordButton.text("Stop");
+            timer = setInterval(function() {
+                gif.addFrame(context, {
+                    delay: frameDelay,
+                    copy: true
+                });
+            }, frameDelay);
+        } else {
+            recordButton.text("Processing...");
+            clearInterval(timer);
+            progressDiv.fadeIn();
+            gif.on('progress', function(progress) {
+                progressbar.css('width', progress * 100 + '%').attr('aria-valuenow', progress * 100);
+                progressbar.html(Math.ceil(progress * 100) + '%');
+            });
+            gif.on('finished', function(blob) {
+                recordButton.text('Record');
+                recording = false;
+                progressDiv.fadeOut();
+                progressbar.css('width', '0%').attr('aria-valuenow', 0);
+                var urlCreator = window.URL || window.webkitURL;
+                var imageUrl = urlCreator.createObjectURL(blob);
+                var reader = new FileReader();
+                reader.readAsDataURL(blob);
+                reader.onloadend = function() {
+                    var displayImg = $('<img class="img-thumbnail">');
+                    displayImg.css('max-width', '100%');
+                    displayImg.css('max-height', '100%');
+                    displayImg.css('margin', '10px 00px 10px 00px');
+                    displayImg.attr('src', reader.result);
 
-          var link = $('<a target="_blank">');
-          link.attr('href', imageUrl);
-          displayImg.appendTo(link);
+                    var link = $('<a target="_blank">');
+                    link.attr('href', imageUrl);
+                    displayImg.appendTo(link);
 
-          var imageLinkDiv = $('#imageLinkDiv');
-          if (!$.trim(imageLinkDiv.html()).length) {
-            var h3 = $('<h3>');
-            h3.html('Recordings');
-            h3.appendTo('#imageLinkDiv');
-          }
-          link.appendTo('#imageLinkDiv');
-        };
-      });
-      gif.render();
-    }
-  });
+                    var imageLinkDiv = $('#imageLinkDiv');
+                    if (!$.trim(imageLinkDiv.html()).length) {
+                        var h3 = $('<h3>');
+                        h3.html('Recordings');
+                        h3.appendTo('#imageLinkDiv');
+                    }
+                    link.appendTo('#imageLinkDiv');
+                };
+            });
+            gif.render();
+        }
+    });
 });
