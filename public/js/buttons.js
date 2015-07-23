9344d1a1f939208e4140955c37640b0f289b5773
Cameron Alexander <emptyflash@gmail.com>
2015-07-23 09:18:54 -0700

    Added some code to attempt to redirect key events to canvas

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -7,24 +7,40 @@ $(document).ready(function() {
 function getProcessingInstance() {
     var bound = false;
     processingInstance = Processing.getInstanceById('feedback');
-    if (processingInstance != null)
-    bound = true;
-    if (!bound)
-    setTimeout(getProcessingInstance, 250);
-    else
-    bindButtons();
+    if (processingInstance != null) {
+        bound = true;
+    }
+    if (!bound) {
+        setTimeout(getProcessingInstance, 250);
+    } else {
+        bindButtons();
+        addListeners();
+    }
 };
 
-function focusCanvas() {
-    $("#feedback").attr("tabindex", 0);
-    $("#feedback").focus();
-};
+function addListeners() {
+    var canvas = document.getElementById("feedback");
+
+    document.addEventListener('keydown', function(event) {
+        event.preventDefault();
+        var newEvent = document.createEvent('KeyboardEvent');
+        (newEvent.initKeyEvent || newEvent.initKeyboardEvent)('keydown', true, true, window,
+            0, 0, 0, 0,
+            0, event.keyCode);
+        canvas.dispatchEvent(newEvent);
+    }, true);
+    document.addEventListener('keyup', function(event) {
+        event.preventDefault();
+        var newEvent = document.createEvent('KeyboardEvent');
+        (newEvent.initKeyEvent || newEvent.initKeyboardEvent)('keyup', true, true, window,
+            0, 0, 0, 0,
+            0, event.keyCode);
+        canvas.dispatchEvent(newEvent);
+    }, true);
+}
 
 function bindButtons() {
-    $("#rotateLeftButton").mousedown(function() { 
-        processingInstance.leftPressed();
-        focusCanvas();
-    });
+    $("#rotateLeftButton").mousedown(function() { processingInstance.leftPressed(); });
     $("#rotateLeftButton").mouseup(function() { processingInstance.leftReleased(); });
     $("#rotateRightButton").mousedown(function() { processingInstance.rightPressed(); });
     $("#rotateRightButton").mouseup(function() { processingInstance.rightReleased(); });
