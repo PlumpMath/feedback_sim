db0f16ff7ff488854bfeb79b4b7d3b605d5bf855
Cameron Alexander <emptyflash@gmail.com>
2015-08-02 15:14:19 -0500

    Successfully made gifs save on server from stream, still very slow

--- index.js
+++ index.js
@@ -25,32 +25,25 @@ var getSessionId = function() {
 };
 
 binaryserver.on('connection', function(client) {
-    client.on('stream', function(stream, meta) {
+    client.on('stream', function(stream) {
         var sessionId = getSessionId();
-        
+
         var encoder = new GIFEncoder(600, 600);
-        encoder.createReadStream().pipe(fs.createWriteStream(sessionId + '.gif'));
+        // encoder.createReadStream().pipe(fs.createWriteStream(sessionId + '.gif'));
 
-        encoder.start();
-        encoder.setRepeat(0);
-        encoder.setFrameRate(30);
-        encoder.setQuality(30);
+        stream.pipe(encoder.createWriteStream({
+            repeat: 0,
+            frameRate: 30,
+            quality: 30
+        })).pipe(fs.createWriteStream(sessionId + '.gif'));
 
-        var jsonData = ""
-        stream.on('data', function(chunk) {
-            if (chunk != null) {
-                jsonData += chunk;
-            } else {
-                var frameSequence = JSON.parse(jsonData).sequence;
-                frameSequence.forEach(function(data, index) {
-                    var imageData = data.replace(/^data:image\/png;base64,/,"");
-                    encoder.addFrame(new Buffer(imageData, 'base64'));
-                });
+        // stream.on('data', function(chunk) {
+        //     console.log(chunk);
+        // });
 
-                encoder.finish();
-
-                stream.end();
-            }
+        stream.on('end', function() {
+            var stream = client.createStream({sessionId: sessionId});
+            stream.end();
         });
     });
 
@@ -62,24 +55,12 @@ binaryserver.on('connection', function(client) {
 app.get('/get-video/:session_id/', function(request, response) {
     var session_id = request.params.session_id.split('.')[0];
 
-    response.writeHead(200, "video/msvideo");
+    response.writeHead(200, 'video/msvideo');
 
     var fileStream = fs.createReadStream('video-' + session_id + '.avi');
     fileStream.pipe(response);
 });
 
-setInterval(function() {
-    console.log("start");
-    fs.readdir(".", function(error, list) {
-        list.forEach(function(file) {
-            var extension = file.split('.')[1];
-            if (extension == "avi" || extension == "png") {
-                fs.unlinkSync(file);
-            }
-        });
-    });
-}, 3600000);
-
 app.listen(app.get('port'), function() {
-    console.log("Feedback Simulator is running at localhost:" + port);
+    console.log('Feedback Simulator is running at localhost:' + port);
 });
--- public/js/record.js
+++ public/js/record.js
@@ -7,104 +7,62 @@ $(document).ready(function() {
     var timer = null;
     var frames = [];
 
-    var recordButton = $("#recordButton");
+    var recordButton = $('#recordButton');
     var progressbar = $('.progress-bar');
     var progressDiv = $('.progress');
     progressDiv.hide();
 
+    var host = location.origin.replace(/^http/, 'ws');
+    var client;
+    var stream;
+
     recordButton.click(function() {
         if (!recording) {
-            recording = true;
-            recordButton.text("Stop");
-            timer = setInterval(function() {
-                frames.push(context.getImageData(0, 0, canvas.width, canvas.height));
-            }, frameDelay);
+            client = new BinaryClient(host);
+            client.on('open', function() {
+                stream = client.createStream();
+                recording = true;
+                recordButton.text('Stop');
+                timer = setInterval(function() {
+                    stream.write(context.getImageData(0, 0, canvas.width, canvas.height).data);
+                }, frameDelay);
+            });
         } else {
-            recordButton.text("Processing...");
+            recordButton.text('Processing...');
             clearInterval(timer);
             progressDiv.fadeIn();
-            var host = location.origin.replace(/^http/, 'ws');
-            var client = new BinaryClient(host);
-            client.on('open', function(stream) {
-                var stream = client.createStream();
+            stream.end();
 
-                frames.forEach(function(frame) {
-                    stream.write(JSON.stringify({frame: frame}));
-                });
-                stream.write(null);
+            client.on('open', function(stream, meta) {
+                recordButton.text('Record');
+                recording = false;
+                progressDiv.fadeOut();
+                progressbar.css('width', '0%').attr('aria-valuenow', 0);
 
-                stream.on('end', function() {
-                    recordButton.text('Record');
-                    recording = false;
-                    progressDiv.fadeOut();
-                    progressbar.css('width', '0%').attr('aria-valuenow', 0);
-                    
-                    var displayImg = $('<img class="img-thumbnail">');
-                    displayImg.css('max-width', '100%');
-                    displayImg.css('max-height', '100%');
-                    displayImg.css('margin', '10px 00px 10px 00px');
-                    // displayImg.attr('src', reader.result);
+                var displayImg = $('<img class="img-thumbnail">');
+                displayImg.css('max-width', '100%');
+                displayImg.css('max-height', '100%');
+                displayImg.css('margin', '10px 00px 10px 00px');
+                // displayImg.attr('src', reader.result);
 
-                    var link = $('<a target="_blank">');
-                    link.attr('href', imageUrl);
-                    displayImg.appendTo(link);
+                var link = $('<a target="_blank">');
+                link.attr('href', imageUrl);
+                displayImg.appendTo(link);
 
-                    var imageLinkDiv = $('#imageLinkDiv');
-                    if (!$.trim(imageLinkDiv.html()).length) {
-                        var h3 = $('<h3>');
-                        h3.html('Recordings');
-                        h3.appendTo('#imageLinkDiv');
-                    }
-                    link.appendTo('#imageLinkDiv');
-                });
-
-                stream.on('error', function(err) {
-                    alert("Problem saving gif!");
-                    recording = false;
-                    recordButton.text("Record");
-                });
+                var imageLinkDiv = $('#imageLinkDiv');
+                if (!$.trim(imageLinkDiv.html()).length) {
+                    var h3 = $('<h3>');
+                    h3.html('Recordings');
+                    h3.appendTo('#imageLinkDiv');
+                }
+                link.appendTo('#imageLinkDiv');
             });
 
             client.on('error', function(err) {
-                alert("Problem saving video!");
+                alert('Problem saving video!');
                 recording = false;
-                recordButton.text("Record");
+                recordButton.text('Record');
             });
-
-            // gif.on('progress', function(progress) {
-            //     progressbar.css('width', progress * 100 + '%').attr('aria-valuenow', progress * 100);
-            //     progressbar.html(Math.ceil(progress * 100) + '%');
-            // });
-            // gif.on('finished', function(blob) {
-            //     recordButton.text('Record');
-            //     recording = false;
-            //     progressDiv.fadeOut();
-            //     progressbar.css('width', '0%').attr('aria-valuenow', 0);
-            //     var urlCreator = window.URL || window.webkitURL;
-            //     var imageUrl = urlCreator.createObjectURL(blob);
-            //     var reader = new FileReader();
-            //     reader.readAsDataURL(blob);
-            //     reader.onloadend = function() {
-            //         var displayImg = $('<img class="img-thumbnail">');
-            //         displayImg.css('max-width', '100%');
-            //         displayImg.css('max-height', '100%');
-            //         displayImg.css('margin', '10px 00px 10px 00px');
-            //         displayImg.attr('src', reader.result);
-
-            //         var link = $('<a target="_blank">');
-            //         link.attr('href', imageUrl);
-            //         displayImg.appendTo(link);
-
-            //         var imageLinkDiv = $('#imageLinkDiv');
-            //         if (!$.trim(imageLinkDiv.html()).length) {
-            //             var h3 = $('<h3>');
-            //             h3.html('Recordings');
-            //             h3.appendTo('#imageLinkDiv');
-            //         }
-            //         link.appendTo('#imageLinkDiv');
-            //     };
-            // });
-            // gif.render();
         }
     });
 });
