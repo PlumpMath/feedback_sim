c74b633301b98ef692cd5d5ad05ed04c2e34ed97
Cameron Alexander <emptyflash@gmail.com>
2015-08-02 16:32:23 -0500

    Added new design and fixed some crashes

--- index.js
+++ index.js
@@ -16,72 +16,9 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
-  response.render('index.html');
+    response.render('index.html');
 });
 
-// binaryserver.on('connection', function(client) {
-// 	session_id = "";
-// 	client.on('stream', function(stream, meta) {
-// 		session_id = meta.session_id;
-// 		jsonData = ""
-// 		stream.on('data', function(chunk) {
-// 			if(chunk != null)
-// 				jsonData += chunk;
-// 			else
-// 			{
-// 				var pngSequence = JSON.parse(jsonData).sequence;
-//
-// 				pngSequence.forEach(function(image, index) {
-// 					var data = image.replace(/^data:image\/png;base64,/,"");
-// 					var indexString = String("0000" + index).slice(-4);
-// 					fs.writeFile('image-' + session_id + '-' + indexString + '.png', data, "base64", function(err) {
-//
-// 					});
-// 				});
-//
-// 				var command = new ffmpeg('image-' + session_id + '-%04d.png')
-// 					.withSize('600x600')
-// 					.withFps(30)
-// 					.withFpsOutput(30)
-// 					.toFormat('avi')
-// 					.on('error', function(err) {
-// 						console.log(err);
-// 					})
-// 					.on('end', function() {
-// 						stream.end();
-// 					})
-// 					.saveToFile('video-' + session_id + '.avi');
-// 			}
-// 		});
-// 	});
-//
-// 	client.on('close', function() {
-//
-// 	});
-// });
-//
-// app.get('/get-video/:session_id/', function(request, response) {
-// 	var session_id = request.params.session_id.split('.')[0];
-//
-// 	response.writeHead(200, "video/msvideo");
-//
-//     var fileStream = fs.createReadStream('video-' + session_id + '.avi');
-//     fileStream.pipe(response);
-// });
-//
-// setInterval(function() {
-// 	console.log("start");
-// 	fs.readdir(".", function(error, list) {
-// 		list.forEach(function(file) {
-// 			var extension = file.split('.')[1];
-// 			if(extension == "avi" || extension == "png")
-// 			{
-// 				fs.unlinkSync(file);
-// 			}
-// 		});
-// 	});
-// }, 3600000);
-
 app.listen(app.get('port'), function() {
-  console.log("Feedback Simulator is running at localhost:" + port);
+    console.log('Feedback Simulator is running at localhost:' + port);
 });
--- public/js/record.js
+++ public/js/record.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     var timer = null;
     var frames = [];
 
-    var recordButton = $("#recordButton");
+    var recordButton = $('#recordButton');
     var progressbar = $('.progress-bar');
     var progressDiv = $('.progress');
     progressDiv.hide();
@@ -17,20 +17,20 @@ $(document).ready(function() {
             gif = new GIF({
                 quality: 45,
                 workers: 4,
-                width: canvas.width,
-                height: canvas.height,
+                width: canvas.width / 2,
+                height: canvas.height / 2,
                 workerScript: 'js/gif.worker.js'
             });
             recording = true;
-            recordButton.text("Stop");
+            recordButton.text('Stop');
             timer = setInterval(function() {
                 gif.addFrame(context, {
                     delay: frameDelay,
                     copy: true
                 });
             }, frameDelay);
-        } else {
-            recordButton.text("Processing...");
+        } else if (recordButton.text() != 'Processing...') {
+            recordButton.text('Processing...');
             clearInterval(timer);
             progressDiv.fadeIn();
             gif.on('progress', function(progress) {
@@ -44,27 +44,24 @@ $(document).ready(function() {
                 progressbar.css('width', '0%').attr('aria-valuenow', 0);
                 var urlCreator = window.URL || window.webkitURL;
                 var imageUrl = urlCreator.createObjectURL(blob);
-                var reader = new FileReader();
-                reader.readAsDataURL(blob);
-                reader.onloadend = function() {
-                    var displayImg = $('<img class="img-thumbnail">');
-                    displayImg.css('max-width', '100%');
-                    displayImg.css('max-height', '100%');
-                    displayImg.css('margin', '10px 00px 10px 00px');
-                    displayImg.attr('src', reader.result);
+                var displayImg = $('<img class="img-thumbnail">');
+                displayImg.css('width', '200px');
+                displayImg.css('height', '200px');
+                displayImg.css('margin', '10px');
+                displayImg.attr('src', imageUrl);
 
-                    var link = $('<a target="_blank">');
-                    link.attr('href', imageUrl);
-                    displayImg.appendTo(link);
+                var link = $('<a target="_blank">');
+                link.attr('href', imageUrl);
+                displayImg.appendTo(link);
 
-                    var imageLinkDiv = $('#imageLinkDiv');
-                    if (!$.trim(imageLinkDiv.html()).length) {
-                        var h3 = $('<h3>');
-                        h3.html('Recordings');
-                        h3.appendTo('#imageLinkDiv');
-                    }
-                    link.appendTo('#imageLinkDiv');
-                };
+                var imageLinkDiv = $('#imageLinkDiv');
+                if (!$.trim(imageLinkDiv.html()).length) {
+                    imageLinkDiv.addClass('well');
+                    var h3 = $('<h2>');
+                    h3.html('Recordings');
+                    h3.appendTo('#imageLinkDiv');
+                }
+                link.appendTo('#imageLinkDiv');
             });
             gif.render();
         }
